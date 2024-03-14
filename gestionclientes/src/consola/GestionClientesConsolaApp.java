602e7ab57d3793778319e7a641442d1679863cd0
Miguel <a_miguel.peraza.pena@iespablopicasso.es>
2024-03-14 22:28:55 +0100

    fin dia 3

--- gestionclientes/src/consola/GestionClientesConsolaApp.java
+++ gestionclientes/src/consola/GestionClientesConsolaApp.java
@@ -1,14 +1,83 @@
 package consola;
 
+import java.util.Scanner;
 
 public class GestionClientesConsolaApp {
 
-  public static void main(String[] args) {
-  
-    
+  private static final String ARCHIVO = "biblioteca.xml";
+
+
+  public GestionClientesConsolaApp() {
+
   }
 
- 
+  public static void main(String[] args) {
+    // Obtenemos los usuarios
+    Scanner sc = new Scanner(System.in);
+    // Creamos un objeto de la clase
+    GestionClientesConsolaApp app = new GestionClientesConsolaApp();
+    // Inicializamos la opción elegida a un valor invalido
+    int opcion =  - 1;
+    // Mientras no se elija una opción correcta
+    do {
+      // Mostramos el menu
+      System.out.println();
+      System.out.println("MENU PRINCIPAL");
+      System.out.println("--------------");
+      System.out.println("1. Listar clientes");
+      System.out.println("2. Buscar clientes por nombre de pila");
+      System.out.println("3. Buscar clientes por apellidos");
+      System.out.println("4. Buscar clientes por DNI");
+      System.out.println("5. Buscar clientes por edad");
+      System.out.println("0. Salir del programa");
+      System.out.print("Elija una opción ( 0 - 4 ): ");
+      try {
+        opcion = Integer.parseInt(sc.nextLine());
+        // Si la opción está en rango se devuelve. Si no se muestra error y se da otra
+        // vuelta
+        if (opcion >= 0 && opcion <= 4) {
+          switch (opcion) {
+          case 1:
+            app.listar();
+            break;
+          case 2:
+            app.añadir();
+            break;
+          case 3:
+            app.actualizar();
+            break;
+          case 4:
+            app.eliminar();
+            break;
 
-  
+          }
+        } else {
+          System.out.println("Opción elegida incorrecta. Debe introducir un número comprendido entre ( 0 - 4 )");
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("Opción elegida incorrecta. Debe introducir un número comprendido entre ( 0 - 4 )");
+      }
+    }while (opcion!=0);
+  }
+
+  private void eliminar() {
+    // TODO Auto-generated method stub
+
+  }
+
+  private void actualizar() {
+    // TODO Auto-generated method stub
+
+  }
+
+  private void añadir() {
+    // TODO Auto-generated method stub
+
+  }
+
+  private void listar() {
+    // TODO Auto-generated method stub
+
+  }
+
 }
--- gestionclientes/src/impl/ProveedorAlmacenamientoClientesFichero.java
+++ gestionclientes/src/impl/ProveedorAlmacenamientoClientesFichero.java
@@ -15,46 +15,85 @@ import core.Cliente;
 import core.ClientesException;
 import core.ProveedorAlmacenamientoClientes;
 
+/*
+ * Clase ProveedorAlmacenamientoClientesFichero: esta clase se se encarga de sacar la información de los clientes de un fichero, además de escrir esta misma y tambien si el fichero no se ecuentra, se crea
+ */
+
 public class ProveedorAlmacenamientoClientesFichero implements ProveedorAlmacenamientoClientes {
 
   // Atributos
   // Almacen de clientes
   private Set<Cliente> clientes;
+  // Ruta que se va a proporcionar
   private String rutaFichero;
 
+  /*
+   * Constructor
+   * 
+   * @param : rutaFichero que esta va a ser la ruta del archivo, o del que se saca
+   * o se escribe la información o del dichero que vamos a crear
+   */
   public ProveedorAlmacenamientoClientesFichero(String rutaFichero) throws IOException {
 
     this.clientes = new HashSet<>();
     this.rutaFichero = rutaFichero;
   }
 
+  /*
+   * Método contains:
+   * 
+   * @return Devuelve tue o false si el contenedor clientes contiene el cliente
+   * dado
+   */
   public boolean contains(Cliente cliente) {
     return clientes.contains(cliente);
   }
 
+  /*
+   * Método size:
+   * 
+   * @return Devuelveel tamaño del contenedor
+   */
   public int size() {
     return clientes.size();
   }
 
+  /*
+   * Método getAll
+   * 
+   * @return Este metodo de vuelve, despues de haber sacado la información del
+   * fichero dado, un contenedor con todos los clientes creados y si no existe el
+   * fichero lo crea
+   */
   @Override
   public Cliente[] getAll() {
+    // Convierte la ruta del fichero de String a path para poder encontrar el
+    // fichero
     Path ruta = Path.of(rutaFichero);
     String linea = null;
     try {
+      // Entra en el archivo, si o enceuntra
       BufferedReader flujoTexto = new BufferedReader(new FileReader(rutaFichero));
+      // Lee el archivo linea a linea hasta acbarlo y saca la información de este para
+      // crear el cliente
       do {
         linea = flujoTexto.readLine();
         if (linea != null) {
+          // Se utiliza split para semapara la linea en datos,ya que estos se separan por
+          // una coma, y los añade a un array de string
           String[] datos = linea.split(",");
+          // Convierte los datos si hay que convertilos y crea el cliente
           Cliente clientesacado = new Cliente(datos[0], datos[1], datos[2], Integer.parseInt(datos[3]),
               Double.parseDouble(datos[4]), Boolean.parseBoolean(datos[5]));
+          // añade el cliente al contenedor que va a devolver
           clientes.add(clientesacado);
         }
       } while (linea != null);
       flujoTexto.close();
       return clientes.toArray(new Cliente[clientes.size()]);
     } catch (FileNotFoundException e) {
-
+      // Si no consigue el archivo, este método lo que hace es que lo crea
+      // devuelve un contenedor vacío
       try {
         Files.createFile(ruta);
       } catch (IOException e1) {
@@ -71,19 +110,21 @@ public class ProveedorAlmacenamientoClientesFichero implements ProveedorAlmacena
 
   @Override
   public void saveAll(Cliente[] clientes) {
-    
-      try {
-        PrintWriter flujoTexto = new PrintWriter(new FileWriter(rutaFichero));
-        for(Cliente clienteAEscribir :clientes ) {
-          flujoTexto.printf("%s, %s, %s, %d, %.2f, %b%n", clienteAEscribir.getNif(), clienteAEscribir.getApellidos(), clienteAEscribir.getNombre(), clienteAEscribir.getEmpleados(), clienteAEscribir.getFacturacion(), clienteAEscribir.isNacionalUe());
-        }
-        flujoTexto.close();
-      } catch (IOException e) {
-        
-        e.printStackTrace();
+    //Entra en el archivo para escribirlo
+    try {
+      PrintWriter flujoTexto = new PrintWriter(new FileWriter(rutaFichero));
+      // De cada cliente que está en el contenedor se sacan los datos y se escriben en
+      // el fichero usanfo el formato del printf
+      for (Cliente clienteAEscribir : clientes) {
+        flujoTexto.printf("%s, %s, %s, %d, %.2f, %b%n", clienteAEscribir.getNif(), clienteAEscribir.getApellidos(),
+            clienteAEscribir.getNombre(), clienteAEscribir.getEmpleados(), clienteAEscribir.getFacturacion(),
+            clienteAEscribir.isNacionalUe());
       }
-      
-   
+      flujoTexto.close();
+    } catch (IOException e) {
+
+      e.printStackTrace();
+    }
 
   }
 
