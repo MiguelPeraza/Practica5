af436712a3cdb4cc6fee44c90867b7dcd671684d
Miguel <a_miguel.peraza.pena@iespablopicasso.es>
2024-03-17 10:00:42 +0100

    base1

--- gestionclientes/src/gui/GestionClientesSwingApp.java
+++ gestionclientes/src/gui/GestionClientesSwingApp.java
@@ -19,6 +19,8 @@ import javax.swing.JCheckBox;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
 
 public class GestionClientesSwingApp extends JFrame {
   private JPanel contentPane;
@@ -29,9 +31,12 @@ public class GestionClientesSwingApp extends JFrame {
   private JTextField txtFacturacion;
   private JList<String> list;
   private JButton btnEliminar;
-  private JButton btnActualizar;
   private JButton btnSalir;
   private JButton btnNuevo;
+  private JCheckBox chckbxNacionalidadEu;
+  private JButton btnActualizar;
+  private JButton btnAceptar;
+  private JButton btnCancelar;
 
   /**
    * Launch the application.
@@ -71,6 +76,36 @@ public class GestionClientesSwingApp extends JFrame {
     contentPane.add(scrollPane);
 
     list = new JList<String>();
+    list.addListSelectionListener(new ListSelectionListener() {
+      @Override
+      public void valueChanged(ListSelectionEvent e) {
+        String[] datos = list.getSelectedValue().split(":");
+        String dni = datos[0];
+        try {
+          ProveedorAlmacenamientoClientesFichero proveedor = new ProveedorAlmacenamientoClientesFichero(
+              "C:\\Users\\maper\\OneDrive\\Escritorio\\Clientes.dat");
+          Cliente[] almacen = proveedor.getAll();
+          for (Cliente cliente : almacen) {
+            if (cliente.getNif().equals(dni)) {
+              txtNif.setText(dni);
+              txtNombre.setText(cliente.getNombre());
+              txtApellidos.setText(cliente.getApellidos());
+              txtEmpleados.setText(String.valueOf(cliente.getEmpleados()));
+              txtFacturacion.setText(String.valueOf(cliente.getFacturacion()));
+              btnEliminar.setEnabled(true);
+              btnActualizar.setEnabled(true);
+              txtNif.setEnabled(false);
+              btnNuevo.setEnabled(false);
+
+            }
+          }
+
+        } catch (IOException e1) {
+          System.out.print("Problemas con el fichero");
+        }
+      }
+    });
+
     DefaultListModel<String> modelo = new DefaultListModel<>();
     try {
       ProveedorAlmacenamientoClientesFichero proveedor = new ProveedorAlmacenamientoClientesFichero(
@@ -93,6 +128,7 @@ public class GestionClientesSwingApp extends JFrame {
     contentPane.add(lblNif);
 
     txtNif = new JTextField();
+    txtNif.setEnabled(false);
     txtNif.setBounds(41, 165, 120, 20);
     contentPane.add(txtNif);
     txtNif.setColumns(10);
@@ -103,6 +139,7 @@ public class GestionClientesSwingApp extends JFrame {
     contentPane.add(lblNombre);
 
     txtNombre = new JTextField();
+    txtNombre.setEnabled(false);
     txtNombre.setColumns(10);
     txtNombre.setBounds(228, 165, 120, 20);
     contentPane.add(txtNombre);
@@ -113,6 +150,7 @@ public class GestionClientesSwingApp extends JFrame {
     contentPane.add(lblApellidos);
 
     txtApellidos = new JTextField();
+    txtApellidos.setEnabled(false);
     txtApellidos.setColumns(10);
     txtApellidos.setBounds(83, 196, 265, 20);
     contentPane.add(txtApellidos);
@@ -123,6 +161,7 @@ public class GestionClientesSwingApp extends JFrame {
     contentPane.add(lblEmpleados);
 
     txtEmpleados = new JTextField();
+    txtEmpleados.setEnabled(false);
     txtEmpleados.setColumns(10);
     txtEmpleados.setBounds(96, 225, 65, 20);
     contentPane.add(txtEmpleados);
@@ -133,6 +172,7 @@ public class GestionClientesSwingApp extends JFrame {
     contentPane.add(lblFacturacion);
 
     txtFacturacion = new JTextField();
+    txtFacturacion.setEnabled(false);
     txtFacturacion.setColumns(10);
     txtFacturacion.setBounds(257, 227, 91, 20);
     contentPane.add(txtFacturacion);
@@ -142,48 +182,96 @@ public class GestionClientesSwingApp extends JFrame {
     lblNacionalidadEu.setBounds(10, 262, 152, 14);
     contentPane.add(lblNacionalidadEu);
 
-    JCheckBox chckbxNacionalidadEu = new JCheckBox("");
+    chckbxNacionalidadEu = new JCheckBox("");
+    chckbxNacionalidadEu.setEnabled(false);
     chckbxNacionalidadEu.setBounds(165, 258, 97, 23);
     contentPane.add(chckbxNacionalidadEu);
 
     btnNuevo = new JButton("Nuevo");
     btnNuevo.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        list.setModel(modelo);
-        try {
-          // Saca la información del archivo
-          ProveedorAlmacenamientoClientesFichero proveedor = new ProveedorAlmacenamientoClientesFichero(
-              "C:\\Users\\maper\\OneDrive\\Escritorio\\Clientes.dat");
-          Clientes almacen = new Clientes(proveedor);
+        btnAceptar.setEnabled(true);
+        txtNif.setEnabled(true);
+        txtNombre.setEnabled(true);
+        txtApellidos.setEnabled(true);
+        txtEmpleados.setEnabled(true);
+        txtFacturacion.setEnabled(true);
+        chckbxNacionalidadEu.setEnabled(true);
+        btnCancelar.setEnabled(true);
+        btnNuevo.setEnabled(false);
+        
+        btnAceptar.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            list.setModel(modelo);
+            try {
+              // Saca la información del archivo
+              ProveedorAlmacenamientoClientesFichero proveedor = new ProveedorAlmacenamientoClientesFichero(
+                  "C:\\Users\\maper\\OneDrive\\Escritorio\\Clientes.dat");
+              Clientes almacen = new Clientes(proveedor);
 
-          try {
-            // Se le piden al usuario los datos
-            String nif = txtNif.getText();
-            String nombre = txtNombre.getText();
-            String apellido = txtApellidos.getText();
-            int empleados =Integer.parseInt(txtEmpleados.getText());
-            double facturacion = Double.parseDouble(txtFacturacion.getText());
-            // Crea y añade el cliente al archivo
-            Cliente cliente = new Cliente(nif, apellido, nombre, empleados, facturacion, true);
-            almacen.addCliente(cliente);
-            modelo.addElement(cliente.getNif() + ": " + cliente.getApellidos() + ", " + cliente.getNombre());
-            System.out.println("Operación realizada con éxito");
-          } catch (IllegalArgumentException e31) {
-            // Se lanza si el dni es incorrecto
-            System.out.print("Ha introducido un dato erroneo");
+              try {
+                // Se le piden al usuario los datos
+                String nif = txtNif.getText();
+                String nombre = txtNombre.getText();
+                String apellido = txtApellidos.getText();
+                int empleados = Integer.parseInt(txtEmpleados.getText());
+                double facturacion = Double.parseDouble(txtFacturacion.getText());
+                Boolean nacionalidadEu = chckbxNacionalidadEu.isSelected();
+                // Crea y añade el cliente al archivo
+                Cliente cliente = new Cliente(nif, apellido, nombre, empleados, facturacion, nacionalidadEu);
+                almacen.addCliente(cliente);
+                modelo.addElement(cliente.getNif() + ": " + cliente.getApellidos() + ", " + cliente.getNombre());
+                System.out.println("Operación realizada con éxito");
+                txtNif.setText("");
+                txtNombre.setText("");
+                txtApellidos.setText("");
+                txtEmpleados.setText("");
+                txtFacturacion.setText("");
+                if (nacionalidadEu.equals(true)) {
+                  chckbxNacionalidadEu.doClick();
+                }
+              } catch (IllegalArgumentException e31) {
+                // Se lanza si el dni es incorrecto
+                System.out.print("Ha introducido un dato erroneo");
 
-          } catch (ClientesException e41) {
-            // Se lanza si el dni esta repetido
-            System.out.print("Ha introducido un dni repetido");
+              } catch (ClientesException e41) {
+                // Se lanza si el dni esta repetido
+                System.out.print("Ha introducido un dni repetido");
+
+              }
+
+            } catch (IOException e1) {
+              e1.printStackTrace();
+            }
 
           }
+        });
+        btnCancelar.addActionListener(new ActionListener() {
 
-        } catch (IOException e1) {
-          e1.printStackTrace();
-        }
-        
+          @Override
+          public void actionPerformed(ActionEvent e) {
+            btnAceptar.setEnabled(false);
+            txtNif.setEnabled(false);
+            txtNombre.setEnabled(false);
+            txtApellidos.setEnabled(false);
+            txtEmpleados.setEnabled(false);
+            txtFacturacion.setEnabled(false);
+            chckbxNacionalidadEu.setEnabled(false);
+            btnCancelar.setEnabled(false);
+            btnNuevo.setEnabled(true);
+            
+          }});
       }
+
     });
+
+    if (list.getSelectedValue() != null) {
+      txtNif.setText("a");
+      txtNombre.setText("a");
+      txtApellidos.setText("a");
+      txtEmpleados.setText("a");
+      txtFacturacion.setText("a");
+    }
     btnNuevo.setFont(new Font("Tahoma", Font.PLAIN, 12));
     btnNuevo.setBounds(10, 295, 89, 23);
     contentPane.add(btnNuevo);
@@ -209,5 +297,15 @@ public class GestionClientesSwingApp extends JFrame {
     btnSalir.setFont(new Font("Tahoma", Font.PLAIN, 12));
     btnSalir.setBounds(307, 295, 89, 23);
     contentPane.add(btnSalir);
+
+    btnAceptar = new JButton("Aceptar");
+    btnAceptar.setEnabled(false);
+    btnAceptar.setBounds(10, 327, 89, 23);
+    contentPane.add(btnAceptar);
+
+    btnCancelar = new JButton("Cancelar");
+    btnCancelar.setEnabled(false);
+    btnCancelar.setBounds(109, 329, 89, 23);
+    contentPane.add(btnCancelar);
   }
 }
